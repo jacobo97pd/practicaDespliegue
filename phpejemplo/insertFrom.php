fe8203629b405ba0beb0e160e9e30632283ca452
Error_Code0x0000022323 <50262343+jacobo97pd@users.noreply.github.com>
2021-02-18 11:47:40 +0100

    Add files via upload

---
+++ phpejemplo/insertFrom.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+  <title>Problema</title>
+</head>
+<body>
+  <form action="insertFrom2.php" method="post">
+    Ingrese id:
+    <input type="text" name="id"><br>
+    Ingrese nombre:
+    <input type="text" name="firstname"><br>
+    Ingrese apellido:
+    <input type="text" name="lastname"><br>
+    Ingrese mail:
+    <input type="text" name="email"><br>
+
+    
+	
+    <br>
+    <input type="submit" value="Registrar">
+  </form>
+</body>
+</html>
